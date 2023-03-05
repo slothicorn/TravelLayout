3f88f0dbc65f18e6990b7bba76c9252068d05427
Michal Bajcar <michal.bajcar@poski.com>
2023-03-05 17:40:04 +0100

    added animations and swiper

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//SWIPER
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
 
 const swiper = new Swiper('.swiper', {
@@ -5,12 +6,14 @@ const swiper = new Swiper('.swiper', {
   spaceBetween: 16,
 
   breakpoints: {
-    400: {
-      slidesPerView: 1.5
+    380: {
+      slidesPerView: 1.5,
+      spaceBetween: 8
     },
 
     600: {
-      slidesPerView: 2
+      slidesPerView: 2,
+      spaceBetween: 10
     },
 
     800: {
@@ -25,3 +28,11 @@ const swiper = new Swiper('.swiper', {
   },
 
 });
+
+//ON LOAD ANIMACE
+
+gsap.to('.home-hero', {autoAlpha: 1, duration: 1.5})
+
+gsap.to('.destination-image', {x: 0, duration: 1})
+
+gsap.to('.destination-description', {x: 0, duration: 1})
